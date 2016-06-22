9e61ecef2387cd61876b3802388bec7f9d152807
s2003zy@qq.com <s2003zy@qq.com>
2016-06-23 00:24:54 +0800

    rm angluar and add grunt-contrib-clean

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,20 +2,27 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        bower: {
-            dev: {
-                dest: 'public/',
-                js_dest: 'public/js/lib',
-                css_dest: 'public/css/lib'
+        pkg : grunt.file.readJSON('package.json'),
+        bower : {
+            dev : {
+                dest        :   'public/',
+                js_dest     :   'public/js/lib',
+                css_dest    :   'public/css/lib'
             }
+        },
+        clean: {
+            css : ['public/css/lib'],
+            js  : ['public/js/lib']
         }
     });
 
-    // 加载包含 "uglify" 任务的插件。
+    // 加载grunt-contrib-clean包
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    // 加载包含 "grunt-bower" 任务的插件。
     grunt.loadNpmTasks('grunt-bower');
 
     // 默认被执行的任务列表。
-    grunt.registerTask('default', ['bower']);
+    grunt.registerTask('default', ['clean', 'bower']);
 
 };
